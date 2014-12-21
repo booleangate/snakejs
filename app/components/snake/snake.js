7823a4266e5e22ce91ffaf66a3187e292886d3a9
Justin Johnson <booleangate@gmail.com>
2014-12-21 13:03:27 -0800

    Setup project skeleton and added stubs

---
+++ app/components/snake/snake.js
@@ -0,0 +1,3 @@
+define(["components/component", "components/snake/head", "components/snake/body"], function(Component, Head, Body) {
+
+});
---
+++ app/snake.js
@@ -0,0 +1,9 @@
+require.config({
+	paths: {
+		underscore: "../vendor/underscore/underscore-min"
+	}
+});
+
+require(["underscore", "components/snake/snake", "components/apple"], function(_, Snake, Apple) {
+	
+});
