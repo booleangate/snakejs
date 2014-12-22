38189f3c7d01f40b4323ac4852249ad6990d9c29
Justin Johnson <booleangate@gmail.com>
2014-12-21 21:42:22 -0800

    Added config, started canvas work

---
+++ app/config.js
@@ -0,0 +1,13 @@
+define(function() {
+	return {
+		colors: {
+			snake: "rgba(127, 175, 27, 1)",
+			apple: "red"
+		},
+		
+		unit: 10,
+		spacing: 1,
+		
+		startingSize: 4,
+	};
+});
--- app/snake.js
+++ app/snake.js
@@ -1,9 +1,22 @@
 require.config({
 	paths: {
-		underscore: "../vendor/underscore/underscore-min"
+		underscore: "../vendor/underscore/underscore-min",
+		domReady: "../vendor/domReady/domReady"
 	}
 });
 
-require(["underscore", "game-objects/snake", "game-objects/apple"], function(_, Snake, Apple) {
+require([
+	"domReady!",
+	"underscore",
+	"config", 
+	"game-objects/snake", 
+	"game-objects/apple"
+], function(_, Config, Snake, Apple) {
+	// Get canvas reference
+	var canvas = document.getElementsByTagName("canvas")[0],
+		ctx = canvas.getContext("2d");
+		
+	// Capture keyboard movement in the canvas
 	
+	// Capture mouse clicks in the canvas	
 });
